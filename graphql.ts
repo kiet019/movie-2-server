8eeec4f44fa085867c81e57f2c0118d16faec3d4
kiet019 <kietly019@gmail.com>
2023-05-25 15:35:27 +0700

    update

--- graphql.ts
+++ graphql.ts
@@ -1,6 +1,7 @@
 import { gql } from "apollo-server";
 import { NewsModel } from "./model/NewsModel";
-import { AddInputNews, UpdateInputNews } from "./interface";
+import { AddInputNews, News, UpdateInputNews } from "./interface";
+import { ObjectId } from "mongodb";
 export const typeDefs = gql`
   type News {
     img: String
@@ -16,7 +17,7 @@ export const typeDefs = gql`
   type Mutation {
     addNews(addInputNews: addInputNews): News
     updateNews(updateInputNews: updateInputNews): Boolean
-    deleteNews(id: String!): Boolean
+    deleteNews(ids: [String!]): Boolean
   }
   input addInputNews {
     img: String
@@ -36,28 +37,42 @@ export const typeDefs = gql`
 export const resolvers = {
   Query: {
     getNews: async (_: any, { id }: { id: String }) => {
-        const response = await NewsModel.findById(id)
-        return response;
+      const response = await NewsModel.findById(id);
+      return response;
     },
     getAllNews: async () => {
-      const response = await NewsModel.find()
+      const response = await NewsModel.find();
       return response;
-  },
+    },
   },
   Mutation: {
-    addNews: async (_: any, { addInputNews }: { addInputNews: AddInputNews }) => {
+    addNews: async (
+      _: any,
+      { addInputNews }: { addInputNews: AddInputNews }
+    ) => {
       const response = await NewsModel.create(addInputNews);
       return response;
     },
-    updateNews:async (_: any, { updateInputNews }: { updateInputNews: UpdateInputNews}) => {
-      const response = await NewsModel.updateOne({ _id: updateInputNews.id } ,  
-        { img: updateInputNews.img, title: updateInputNews.title, by: updateInputNews.by, description: updateInputNews.description }
-      )
-      return response.modifiedCount > 0 ? true : false
+    updateNews: async (
+      _: any,
+      { updateInputNews }: { updateInputNews: UpdateInputNews }
+    ) => {
+      const response = await NewsModel.updateOne(
+        { _id: updateInputNews.id },
+        {
+          img: updateInputNews.img,
+          title: updateInputNews.title,
+          by: updateInputNews.by,
+          description: updateInputNews.description,
+        }
+      );
+      return response.modifiedCount > 0 ? true : false;
     },
-    deleteNews: async (_: any, { id }: { id: String}) => {
-      const response = await NewsModel.deleteOne({ _id: id })
-      return response.deletedCount > 0 ? true : false
-    }
+    deleteNews: async (_: any, { ids }: { ids: string[] }) => {
+      const objectIds = ids.map(id => new ObjectId(id));
+      const filter = { _id: { $in: objectIds } };
+      const response = await NewsModel.deleteMany(filter)      
+      return response.deletedCount > 0 ? true : false;
+    },
   },
 };
